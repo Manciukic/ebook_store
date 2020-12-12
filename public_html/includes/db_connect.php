f0ec201ff0b4289a75ce2f181d5a20949f5ccfbb
Riccardo Mancini <r.mancini@santannapisa.it>
2020-12-12 20:28:23 +0100

    db_connect: raise exception on query error

--- public_html/includes/db_connect.php
+++ public_html/includes/db_connect.php
@@ -5,6 +5,9 @@
 
     require_once("includes/settings.php");
 
+    // raise exceptions in case of errors
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
     $mysqli = new mysqli($db_host, $db_username, $db_password, $db_name);
     if ($mysqli->connect_errno) {
         echo "We are experiencing problems. Try again later!";
