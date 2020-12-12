eaee63d4dc7d5e9660f1fa02596fc5042cb8c2b4
Riccardo Mancini <r.mancini@santannapisa.it>
2020-12-12 19:56:29 +0100

    checkout: validate credit card attributes

--- public_html/includes/checkout_1_payment.php
+++ public_html/includes/checkout_1_payment.php
@@ -22,32 +22,45 @@ if (!isset($_SESSION['user_id'])) {
 }
 
 $_SESSION['stage'] = 1;
+require_once "includes/validation_functions.php";
 
+$errors = array();
 
-// TODO check inputs
-if (isset($_POST["card_id"])){
-    if ($_POST["card_id"] == ""){
+if (isset($_POST["card_id"]) || isset($_POST["card_number"])){
+    if (empty($_POST["card_id"])){
         if (!(isset($_POST["card_number"]) && isset($_POST["card_expiration"]) && isset($_POST["card_cvv"]))){
-            // Missing card information
-            $error_code=400;
-            $error_msg="Bad Request: you need to provide all card data";
-            include "includes/error.php";
-            exit;
+            $errors[] = "Plase fill all card information";
+        } else {
+            $card_number_valid = validate_card_number($_POST["card_number"]);
+            if ($card_number_valid !== true)
+                $errors[] = $card_number_valid;
+
+            $card_expiration_valid = validate_card_expiration($_POST["card_expiration"]);
+            if ($card_expiration_valid !== true)
+                $errors[] = $card_expiration_valid;
+
+            $card_cvv_valid = validate_card_cvv($_POST["card_cvv"]);
+            if ($card_cvv_valid !== true)
+                $errors[] = $card_cvv_valid;
         }
 
-        $insert_card_query = $mysqli->prepare(
-            "INSERT INTO credit_cards (`user_id`, `number`, `expiration`, `cvv`) 
-                VALUES (?,?,?,?)"
-        );
-        $insert_card_query->bind_param("isss", $_SESSION['user_id'], $_POST["card_number"], $_POST["card_expiration"], $_POST["card_cvv"]);
-        $insert_card_query->execute();
-        $_SESSION['card_id'] = $mysqli->insert_id;
+        if (empty($errors)){
+            $insert_card_query = $mysqli->prepare(
+                "INSERT INTO credit_cards (`user_id`, `number`, `expiration`, `cvv`) 
+                    VALUES (?,?,?,?)"
+            );
+            $insert_card_query->bind_param("isss", $_SESSION['user_id'], $_POST["card_number"], $_POST["card_expiration"], $_POST["card_cvv"]);
+            $insert_card_query->execute();
+            $_SESSION['card_id'] = $mysqli->insert_id;
+        }
     } else {
         $_SESSION['card_id'] = intval($_POST["card_id"]);
     }
 
-    include "includes/checkout_2_confirmation.php";
-    exit;
+    if (empty($errors)){
+        include "includes/checkout_2_confirmation.php";
+        exit;
+    } // in case of error re-display page with errors
 }
 
 $card_result = get_credit_cards($_SESSION['user_id']);
@@ -66,6 +79,17 @@ $card_result = get_credit_cards($_SESSION['user_id']);
 
 <body>
     <?php include "includes/header.php" ?>
+
+    <?php if (!empty($errors)) { ?>
+        <div>
+            <h3>Please fix these errors</h3>
+            <ul>
+                <?php foreach($errors as $error_msg){ ?>
+                    <li> <?php echo $error_msg ?> </li>
+                <?php } ?>
+            </ul>
+        </div>
+    <?php } ?>
     
     <!-- TODO riepilogo -->
 
