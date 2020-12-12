a98b49fed1fc786932689ceecd43268c8d2e2423
Mirko <mirkoletto@gmail.com>
2020-12-12 19:36:29 +0100

    UX for order confirmation

--- public_html/includes/checkout_2_confirmation.php
+++ public_html/includes/checkout_2_confirmation.php
@@ -1,7 +1,7 @@
 <?php
 
 // check if user has items in cart
-if (empty($_SESSION['items'])){
+if (empty($_SESSION['items'])) {
     // empty cart: how the hell did he get here?
     include "includes/error.php";
     return;
@@ -24,8 +24,8 @@ if (!isset($_SESSION['card_id'])) {
 $_SESSION['stage'] = 2;
 
 // TODO check inputs
-if (isset($_POST["action"])){
-    if ($_POST["action"] == "Confirm"){
+if (isset($_POST["action"])) {
+    if ($_POST["action"] == "Confirm") {
         // Useful variables for preparing variable length statements
         $book_ids = $_SESSION['items'];
         $nbooks =  count($book_ids);
@@ -34,14 +34,14 @@ if (isset($_POST["action"])){
         /* Start transaction */
         $mysqli->begin_transaction();
 
-        try{
+        try {
             $insert_order_query = $mysqli->prepare("
                     INSERT INTO orders (`user_id`, `credit_card_id`, `time`, `price`)
                     SELECT ?, ?, NOW(), SUM(E.price)
                     FROM ebooks E
                     WHERE id IN (" . $book_ids_query . ")
                 ");
-            $insert_order_query->bind_param(str_repeat('i', $nbooks+2), $_SESSION["user_id"], $_SESSION["card_id"], ...$book_ids);
+            $insert_order_query->bind_param(str_repeat('i', $nbooks + 2), $_SESSION["user_id"], $_SESSION["card_id"], ...$book_ids);
             $insert_order_query->execute();
 
             $order_id = $mysqli->insert_id;
@@ -52,13 +52,13 @@ if (isset($_POST["action"])){
                     FROM ebooks E
                     WHERE id IN (" . $book_ids_query . ")
                 ");
-            $insert_order_items_query->bind_param(str_repeat('i', $nbooks+1), $order_id, ...$book_ids);
+            $insert_order_items_query->bind_param(str_repeat('i', $nbooks + 1), $order_id, ...$book_ids);
             $insert_order_items_query->execute();
 
             $mysqli->commit();
         } catch (mysqli_sql_exception $exception) {
             $mysqli->rollback();
-        
+
             throw $exception; // TODO ?
         }
 
@@ -67,7 +67,7 @@ if (isset($_POST["action"])){
         unset($_SESSION["card_id"]);
         header("Location: order.php?id=$order_id");
         return;
-    } else{
+    } else {
         $_SESSION['stage'] = 0;
         header("Location: cart.php");
         return;
@@ -76,7 +76,7 @@ if (isset($_POST["action"])){
 
 $card_result = get_credit_card($_SESSION['card_id']);
 
-if ($card_result->num_rows === 0){
+if ($card_result->num_rows === 0) {
     // Card not found
 
     include "includes/error.php";
@@ -99,13 +99,21 @@ $card = $card_result->fetch_array();
 
 <body>
     <?php include "includes/header.php" ?>
-    
+
     <!-- TODO riepilogo -->
-    You're paying with: **** **** **** <?php echo $card["last_digits"] ?> (<?php echo $card["expiration"] ?>)<br>
-    
-    <form action="checkout.php" method="post">
-    <input type="submit" name="action" value="Confirm">
-    <input type="submit" name="action" value="Abort">
+
+    <form action="checkout.php" method="post" class="stage-form">
+        <p class="confirm-purchase-text">
+            You're paying with
+            <span class="confirm-credit-card">
+                **** **** **** <?php echo $card["last_digits"] ?> (<?php echo $card["expiration"] ?>)
+            </span>
+        </p>
+        <div class="confirm-buttons">
+            <button type="submit" name="action" value="Confirm">Confirm</button>
+            <button type="submit" name="action" value="Abort">Cancel</button>
+        </div>
     </form>
 </body>
+
 </html>
