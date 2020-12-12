d61113e122bc4f136e9f1794483400f316586516
Riccardo Mancini <r.mancini@santannapisa.it>
2020-12-12 18:23:13 +0100

    Use settings file for DB credentials

--- public_html/includes/db_connect.php
+++ public_html/includes/db_connect.php
@@ -2,7 +2,10 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-    $mysqli = new mysqli('127.0.0.1', 'ebook_store', 'k1l3gg3mu0r3', 'EbookStore');
+
+    require_once("includes/settings.php");
+
+    $mysqli = new mysqli($db_host, $db_username, $db_password, $db_name);
     if ($mysqli->connect_errno) {
         echo "We are experiencing problems. Try again later!";
         exit;
