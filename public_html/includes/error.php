036efae85a4c78cb7aa3b7643e1b6f36c64319f7
Mirko <mirkoletto@gmail.com>
2020-12-17 12:17:19 +0100

    Error page now includes error_title

--- public_html/includes/error.php
+++ public_html/includes/error.php
@@ -5,21 +5,25 @@
     if (!isset($error_msg))
         $error_msg = "We are experiencing problems. Try again later!";
     
+    if (!isset($error_title)){
+        $error_title = "Error ".$error_code;
+    }
+
     http_response_code($error_code);
 ?>
 <!DOCTYPE html>
 <html>
 
 <head>
-    <title> Error <?php echo $error_code ?> </title>
+    <title><?php echo $error_title ?></title>
     <?php include "includes/include.php" ?>
 </head>
 
 <body>
     <?php include "includes/header.php" ?>
-    <main class="search-page">
-        <h1>Error <?php echo $error_code ?></h1>
-        <h2><?php echo $error_msg ?></h2>
+    <main class="error-page">
+        <h1><?php echo $error_title ?></h1>
+        <?php echo $error_msg ?>
     </main>
 </body>
 
