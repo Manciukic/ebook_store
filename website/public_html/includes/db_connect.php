4c5d4753fdd6fc800f1a598feb65f05862785266
Riccardo Mancini <r.mancini@santannapisa.it>
2021-01-15 11:32:06 +0100

    improve error handling

--- website/public_html/includes/db_connect.php
+++ website/public_html/includes/db_connect.php
@@ -1,12 +1,13 @@
 <?php
-    require_once("includes/settings.php");
+    require_once "includes/settings.php";
+    require_once "includes/error.php";
 
     // raise exceptions in case of errors
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
     $mysqli = new mysqli($db_host, $db_username, $db_password, $db_name);
     if ($mysqli->connect_errno) {
-        echo "We are experiencing problems. Try again later!";
+        fatal_error_page();
         exit;
     }
 ?>
--- website/public_html/includes/error.php
+++ website/public_html/includes/error.php
@@ -29,4 +29,56 @@ function error_page($error_code, $error_msg){
     // Forcefully exit
     exit;
 }
+
+function fatal_error_page(){
+?>
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <title>Internal Server Error</title>
+        <?php include "includes/include.php" ?>
+    </head>
+
+    <body>
+        <header>
+
+        <a href="index.php">
+            <h1>
+                SecureLibrary
+            </h1>
+            <h2>
+                Your favourite books. Anywhere, anytime
+            </h2>
+        </a>
+        </header>
+        <main class="error-page">
+            <h1>Error 500</h1>
+            <p>There was an error. Please retry later.</p>
+        </main>
+    </body>
+
+    </html>
+<?php
+}
+
+function shutdown_handler(){
+    $lasterror = error_get_last();
+    if ($lasterror === null || !isset($lasterror['type']))
+        return;
+    switch ($lasterror['type'])
+    {
+        case E_ERROR:
+        case E_CORE_ERROR:
+        case E_COMPILE_ERROR:
+        case E_USER_ERROR:
+        case E_RECOVERABLE_ERROR:
+        case E_CORE_WARNING:
+        case E_COMPILE_WARNING:
+        case E_PARSE:
+            fatal_error_page();
+    }
+}
+
+register_shutdown_function("shutdown_handler");
 ?>
