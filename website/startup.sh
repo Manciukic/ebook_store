68d6a5c9e470f3cf737376f5471179debac10a44
Riccardo Mancini <r.mancini@santannapisa.it>
2021-01-15 10:56:59 +0100

    revert dockerfile and startup.sh to main

--- website/startup.sh
+++ website/startup.sh
@@ -1,1 +1,8 @@
+cd /
+hostname=$(hostname)
+line="127.0.0.1 localhost.localdomain localhost $hostname"
+sed -e "1 s/^.*$/${line}/g" /etc/hosts > tmphosts
+cp tmphosts /etc/hosts
+service sendmail restart
+
 /usr/sbin/apache2ctl -D FOREGROUND
